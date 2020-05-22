efafe4edc7947f178b43cd1ecbf11bb89c0a0d7b
Phil <phil@local.git>
2020-05-21 23:50:57 -0500

    adding images and updating html layout for content

--- js/ingaProfile.js
+++ js/ingaProfile.js
@@ -1,35 +1,71 @@
 new Vue({
-  el: '#app',
-  data: {
-    renderingHome: true,
-    renderingMusic: false,
-    renderingArt: false,
-    renderingDance: false
-  },
-  methods: {
-    renderHome: function() {
-      this.renderingHome = true;
-      this.renderingMusic = false;
-      this.renderingArt = false;
-      this.renderingDance = false;
+    el: '#app',
+    data: {
+        renderingAbout: true,
+        renderingMusic: false,
+        renderingArt: false,
+        renderingBodyArt: false,
+        renderingDance: false,
+        traditionalArtImages: [
+            'traditionalArt1.jpg',
+            'traditionalArt2.jpg',
+            'traditionalArt3.jpg',
+            'traditionalArt4.jpg',
+            'traditionalArt5.jpg',
+            'traditionalArt6.jpg',
+            'traditionalArt7.jpg',
+            'traditionalArt8.jpg',
+            'traditionalArt9.jpg',
+            'traditionalArt10.jpg',
+            'traditionalArt11.jpg',
+            'traditionalArt12.jpg',
+            'traditionalArt13.jpg',
+            'traditionalArt14.jpg'
+        ],
+        bodyArtImages: [
+            'bodyArt1.jpg',
+            'bodyArt2.jpg',
+            'bodyArt3.jpg',
+            'bodyArt4.jpg',
+            'bodyArt5.jpg',
+            'bodyArt6.jpg'
+        ]
     },
-    renderMusic: function() {
-      this.renderingHome = false;
-      this.renderingMusic = true;
-      this.renderingArt = false;
-      this.renderingDance = false;
-    },
-    renderArt: function() {
-      this.renderingHome = false;
-      this.renderingMusic = false;
-      this.renderingArt = true;
-      this.renderingDance = false;
-    },
-    renderDance: function() {
-      this.renderingHome = false;
-      this.renderingMusic = false;
-      this.renderingArt = false;
-      this.renderingDance = true;
+    methods: {
+        renderAbout: function () {
+            this.renderingAbout = true;
+            this.renderingMusic = false;
+            this.renderingArt = false;
+            this.renderingBodyArt = false;
+            this.renderingDance = false;
+        },
+        renderMusic: function () {
+            this.renderingAbout = false;
+            this.renderingMusic = true;
+            this.renderingArt = false;
+            this.renderingBodyArt = false;
+            this.renderingDance = false;
+        },
+        renderArt: function () {
+            this.renderingAbout = false;
+            this.renderingMusic = false;
+            this.renderingArt = true;
+            this.renderingBodyArt = false;
+            this.renderingDance = false;
+        },
+        renderBodyArt: function () {
+            this.renderingAbout = false;
+            this.renderingMusic = false;
+            this.renderingArt = false;
+            this.renderingBodyArt = true;
+            this.renderingDance = false;
+        },
+        renderDance: function () {
+            this.renderingAbout = false;
+            this.renderingMusic = false;
+            this.renderingArt = false;
+            this.renderingBodyArt = false;
+            this.renderingDance = true;
+        }
     }
-  }
 })
