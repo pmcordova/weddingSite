d59088ead2ca847786d3e04d52091df4e5c37dbb
Phil <phil@local.git>
2020-05-23 21:06:48 -0500

    initial page styling

--- js/ingaProfile.js
+++ js/ingaProfile.js
@@ -7,65 +7,66 @@ new Vue({
         renderingBodyArt: false,
         renderingDance: false,
         traditionalArtImages: [
-            'traditionalArt1.jpg',
-            'traditionalArt2.jpg',
-            'traditionalArt3.jpg',
-            'traditionalArt4.jpg',
-            'traditionalArt5.jpg',
-            'traditionalArt6.jpg',
-            'traditionalArt7.jpg',
-            'traditionalArt8.jpg',
-            'traditionalArt9.jpg',
-            'traditionalArt10.jpg',
-            'traditionalArt11.jpg',
-            'traditionalArt12.jpg',
-            'traditionalArt13.jpg',
-            'traditionalArt14.jpg'
+            '../img/traditionalArt1.jpg',
+            '../img/traditionalArt2.jpg',
+            '../img/traditionalArt3.jpg',
+            '../img/traditionalArt4.jpg',
+            '../img/traditionalArt5.jpg',
+            '../img/traditionalArt6.jpg',
+            '../img/traditionalArt7.jpg',
+            '../img/traditionalArt8.jpg',
+            '../img/traditionalArt9.jpg',
+            '../img/traditionalArt10.jpg',
+            '../img/traditionalArt11.jpg',
+            '../img/traditionalArt12.jpg',
+            '../img/traditionalArt13.jpg',
+            '../img/traditionalArt14.jpg'
         ],
         bodyArtImages: [
-            'bodyArt1.jpg',
-            'bodyArt2.jpg',
-            'bodyArt3.jpg',
-            'bodyArt4.jpg',
-            'bodyArt5.jpg',
-            'bodyArt6.jpg'
-        ]
+            '../img/bodyArt1.jpg',
+            '../img/bodyArt2.jpg',
+            '../img/bodyArt3.jpg',
+            '../img/bodyArt4.jpg',
+            '../img/bodyArt5.jpg',
+            '../img/bodyArt6.jpg'
+        ],
+        //carouselJS
+        imageCarousel: true,
+        selectedItemIndex: 0,
+        //TODO: see if this method works
+        //selectedItem: this.bodyArtImages[this.selectedItemIndex]
     },
     methods: {
         renderAbout: function () {
             this.renderingAbout = true;
             this.renderingMusic = false;
             this.renderingArt = false;
-            this.renderingBodyArt = false;
             this.renderingDance = false;
         },
         renderMusic: function () {
             this.renderingAbout = false;
             this.renderingMusic = true;
             this.renderingArt = false;
-            this.renderingBodyArt = false;
             this.renderingDance = false;
         },
         renderArt: function () {
             this.renderingAbout = false;
             this.renderingMusic = false;
             this.renderingArt = true;
-            this.renderingBodyArt = false;
-            this.renderingDance = false;
-        },
-        renderBodyArt: function () {
-            this.renderingAbout = false;
-            this.renderingMusic = false;
-            this.renderingArt = false;
-            this.renderingBodyArt = true;
             this.renderingDance = false;
         },
         renderDance: function () {
             this.renderingAbout = false;
             this.renderingMusic = false;
             this.renderingArt = false;
-            this.renderingBodyArt = false;
             this.renderingDance = true;
+        },
+        //carousel JS
+        nextImage: function() {
+            this.selectedItemIndex = Math.min(this.selectedItemIndex + 1, this.bodyArtImages - 1)
+        },
+        previousImage: function() {
+            this.selectedItemIndex = Math.max(0, this.selectedItemIndex - 1)
         }
     }
 })
