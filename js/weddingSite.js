e92ac6a12f5459a8e1e1dc5b06b757c7e096e585
Philip Cordova <G626314@genmills.com>
2021-03-30 16:54:35 -0500

    more changes

--- js/weddingSite.js
+++ js/weddingSite.js
@@ -1,70 +1,25 @@
 new Vue({
     el: '#app',
     data: {
-        renderingAbout: true,
-        renderingMusic: false,
-        renderingArt: false,
-        renderingBodyArt: false,
-        renderingDance: false,
-        traditionalArtImages: [
-            '../img/traditionalArt1.jpg',
-            '../img/traditionalArt2.jpg',
-            '../img/traditionalArt3.jpg',
-            '../img/traditionalArt4.jpg',
-            '../img/traditionalArt5.jpg',
-            '../img/traditionalArt6.jpg',
-            '../img/traditionalArt7.jpg',
-            '../img/traditionalArt8.jpg',
-            '../img/traditionalArt9.jpg',
-            '../img/traditionalArt10.jpg',
-            '../img/traditionalArt11.jpg',
-            '../img/traditionalArt12.jpg',
-            '../img/traditionalArt13.jpg',
-            '../img/traditionalArt14.jpg'
-        ],
-        bodyArtImages: [
-            '../img/bodyArt1.jpg',
-            '../img/bodyArt2.jpg',
-            '../img/bodyArt3.jpg',
-            '../img/bodyArt4.jpg',
-            '../img/bodyArt5.jpg',
-            '../img/bodyArt6.jpg'
-        ],
-        //carouselJS
-        imageCarousel: true,
-        selectedBodyArtIndex: 0
+        currentPage: "HOME",
+        backgroundImage: "../img/home_background.png"
     },
     methods: {
-        renderAbout: function () {
-            this.renderingAbout = true;
-            this.renderingMusic = false;
-            this.renderingArt = false;
-            this.renderingDance = false;
+        renderHomePage: function () {
+            this.currentPage = "HOME";
+            this.backgroundImage = "../img/home_background.png"
         },
-        renderMusic: function () {
-            this.renderingAbout = false;
-            this.renderingMusic = true;
-            this.renderingArt = false;
-            this.renderingDance = false;
+        renderCeremonyPage: function () {
+            this.currentPage = "CEREMONY";
         },
-        renderArt: function () {
-            this.renderingAbout = false;
-            this.renderingMusic = false;
-            this.renderingArt = true;
-            this.renderingDance = false;
+        renderReceptionPage: function () {
+            this.currentPage = "RECEPTION";
         },
-        renderDance: function () {
-            this.renderingAbout = false;
-            this.renderingMusic = false;
-            this.renderingArt = false;
-            this.renderingDance = true;
+        renderRegistryPage: function () {
+            this.currentPage = "REGISTRY";
         },
-        //carousel JS
-        nextImage: function() {
-            this.selectedBodyArtIndex = Math.min(this.selectedBodyArtIndex + 1, this.bodyArtImages.length - 1);
-        },
-        previousImage: function() {
-            this.selectedBodyArtIndex = Math.max(0, this.selectedBodyArtIndex - 1);
+        renderRsvpPage: function() {
+            this.currentPage = "RSVP";
         }
     }
 })
